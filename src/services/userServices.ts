ec13ad7e24d2fa08f49184883474e36fd99d5d19
djembaraa <djembararafat98@gmail.com>
2025-10-30 20:23:13 +0700

    add bcrypt

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,10 +1,18 @@
 import { SignUpValues } from "../utils/schema/user";
 import * as userRepositories from "../repositories/userRepositories";
+import bcrypt from "bcrypt";
 
 export const signUp = async (data: SignUpValues, file: Express.Multer.File) => {
-    const isEmailExist = await userRepositories.isEmailExist(data.email)
+  const isEmailExist = await userRepositories.isEmailExist(data.email);
 
-    if (isEmailExist > 1) {
-        throw new Error("Email already registered")
-    }
-}
+  if (isEmailExist > 1) {
+    throw new Error("Email already registered");
+  }
+  const user = await userRepositories.createUser(
+    {
+      ...data,
+      password: bcrypt.hashSync(data.password, 12),
+    },
+    file.filename
+  );
+};
