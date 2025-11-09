fcd08116f3c78e6b3c9a11eb00c2cc68c5fda3bd
djembaraa <djembararafat98@gmail.com>
2025-11-09 13:23:55 +0700

    update backend 2

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import multer from "multer";
+import { storageUserPhoto } from "../utils/multer";
+import * as userController from "../controllers/userControllers";
+
+const userRouter = express.Router();
+
+const uploadPhoto = multer({
+  storage: storageUserPhoto,
+  fileFilter(req, file, callback) {
+    if (file.mimetype.startsWith("image/")) {
+      callback(null, false);
+    }
+
+    callback(null, true);
+  },
+});
+
+userRouter.post(
+  "auth/sign-up",
+  uploadPhoto.single("profilePicture"),
+  userController.signUp
+);
+
+export default userRouter;
