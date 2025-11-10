8dd30c82645d8d28cc4d6214bd12e6e8a0e38c9f
djembaraa <djembararafat98@gmail.com>
2025-11-10 22:37:33 +0700

    create group v1

---
+++ src/repositories/groupRepositories.ts
@@ -0,0 +1,34 @@
+import prisma from "../utils/prisma";
+import { GroupFreeValues } from "../utils/schema/group";
+import * as userRepositories from "./userRepositories";
+
+export const createGroup = async (
+  data: GroupFreeValues,
+  photo: string,
+  userId: string
+) => {
+  const owner = await userRepositories.findRole("OWNER");
+
+  return await prisma.group.create({
+    data: {
+      photo: photo,
+      name: data.name,
+      about: data.about,
+      price: 0,
+      type: "FREE",
+      room: {
+        create: {
+          created_by: userId,
+          name: data.name,
+          member: {
+            create: {
+              user_id: userId,
+              role_id: owner.id,
+            },
+          },
+          is_group: true,
+        },
+      },
+    },
+  });
+};
